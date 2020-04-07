0e29dbbc9494aa612f8506506bbdc1516823793a
wuss <XdasA2J9zK_sRJfA8HrW>
2020-04-07 14:35:18 +0800

    48、旋转图像

---
+++ src/com.wuss.leetCode/Main48.java
@@ -0,0 +1,102 @@
+package com.wuss.leetCode;
+
+/**
+ * @program leetcode
+ * @description:
+ * @author: wuss@wjs.com
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ * <p>
+ * 将图像顺时针旋转 90 度。
+ * <p>
+ * 说明：
+ * <p>
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ]
+ * 示例 2:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [ 5, 1, 9,11],
+ * [ 2, 4, 8,10],
+ * [13, 3, 6, 7],
+ * [15,14,12,16]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [15,13, 2, 5],
+ * [14, 3, 4, 1],
+ * [12, 6, 8, 9],
+ * [16, 7,10,11]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-image
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main48 {
+    //N*N矩阵
+    public void rotate(int[][] matrix) {
+        //1.又对角线对折
+        //2.上下对称
+        int n = matrix.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][n - i - 1];
+                matrix[n - j - 1][n - i - 1] = temp;
+            }
+        }
+
+        display(matrix);
+        for (int i = 0; i <n/2; i++) {
+            for (int j = 0; j < n; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        display(matrix);
+    }
+
+    public void display(int[][] matrix) {
+        int len = matrix.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------");
+    }
+
+    public static void main(String[] args) {
+        int k = 1;
+        int[][] arr = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = k++;
+            }
+        }
+        Main48 main48 = new Main48();
+        main48.display(arr);
+        main48.rotate(arr);
+    }
+
+}
