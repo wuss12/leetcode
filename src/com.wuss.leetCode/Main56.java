b9e238e0b119884eb79ed34db9a658fc598ba09f
wuss <XdasA2J9zK_sRJfA8HrW>
2020-04-13 13:40:17 +0800

    合并区间

--- src/com.wuss.leetCode/Main56.java
+++ src/com.wuss.leetCode/Main56.java
@@ -1,10 +1,7 @@
 package com.wuss.leetCode;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @program leetcode
@@ -25,7 +22,75 @@ import java.util.Scanner;
  * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
  */
 public class Main56 {
+    class Point {
+        int x,y;
+        public Point(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+        public int[] changeToArr(){
+            return new int[]{x,y};
+        }
+
+
+    }
+
     public int[][] merge(int[][] intervals) {
+        int len = intervals.length;
+        if (len <= 1){
+            return intervals;
+        }
+        List<Point> list = new ArrayList<>(len);
+        for (int i=0;i<len;i++){
+            list.add(new Point(intervals[i][0],intervals[i][1]));
+        }
+        Collections.sort(list, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                if (o1.x != o2.x){
+                    return o1.x - o2.x;
+                }
+                return o1.y - o2.y;
+            }
+        });
+        List<Point> pointsList = new ArrayList<>();
+        Point pOld = null,pNew;
+        for (int i=0;i<len;i++){
+            pNew = list.get(i);
+            if (pOld == null){
+                pOld = pNew;
+                pointsList.add(pOld);
+                continue;
+            }
+
+            if (pNew.x >= pOld.x && pNew.x<= pOld.y && pNew.y >= pOld.y){
+                pOld.y = pNew.y;
+                continue;
+            }
+            if (pNew.y <= pOld.y){
+                continue;
+            }
+            pOld = pNew;
+            pointsList.add(pOld);
+
+        }
+        int[][] arr = new int[pointsList.size()][];
+        for (int i=0;i<arr.length;i++){
+            arr[i] = pointsList.get(i).changeToArr();
+        }
+
+        return arr;
+    }
+
+    public int[][] merge1(int[][] intervals) {
         if (intervals.length <= 1){
             return intervals;
         }
@@ -66,7 +131,11 @@ public class Main56 {
 //        int[][] ints = {{1,3},{2,6},{8,10},{15,18}};
 //        int[][] ints = {{1,4},{4,6},{5,15},{15,18}};
 //        int[][] ints = {{2,3},{4,5},{6,7},{8,9},{1,10}};
-        int[][] ints = {{1,4},{4,5}};
-        System.out.println(main56.merge(ints));
+        int[][] ints = {{1,4},{2,3}};
+        int[][] merge = main56.merge(ints);
+        for (int i=0;i<merge.length;i++){
+            System.out.print("["+merge[i][0]+","+merge[i][1]+"],");
+        }
+        System.out.println();
     }
 }
