7523b6017be45e1026ce3d90678f2771b2a9f6fa
wuss <XdasA2J9zK_sRJfA8HrW>
2020-04-15 15:23:04 +0800

    62. 不同路径

---
+++ src/com.wuss.leetCode/Main62.java
@@ -0,0 +1,56 @@
+package com.wuss.leetCode;
+
+/**
+ * @program leetcode
+ * @description:
+ * @author: wuss@wjs.com
+ * 62. 不同路径
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ *
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ *
+ * 问总共有多少条不同的路径？
+ *
+ *
+ *
+ * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: m = 3, n = 2
+ * 输出: 3
+ * 解释:
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向右 -> 向下
+ * 2. 向右 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向右
+ * 示例 2:
+ *
+ * 输入: m = 7, n = 3
+ * 输出: 28
+ */
+public class Main62 {
+    public int uniquePaths(int m, int n) {
+        int[][] arr = new int[m][n];
+        for (int i=0;i<m;i++){
+            arr[i][0] = 1;
+        }
+        for (int i=0;i<n;i++){
+            arr[0][i] =1;
+        }
+        arr[0][0] = 1;
+        for (int i=1;i< m;i++){
+            for (int j=1;j< n;j++){
+                arr[i][j] = arr[i-1][j]+arr[i][j-1];
+            }
+        }
+        return arr[m-1][n-1];
+
+    }
+
+    public static void main(String[] args) {
+        TestMain.testMethod(Main62.class);
+    }
+}
