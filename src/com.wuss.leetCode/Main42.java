4b5f0193fe1227566d3dc9ae43d264fda0595bc4
wuss <XdasA2J9zK_sRJfA8HrW>
2020-04-26 15:21:41 +0800

    42. 接雨水

---
+++ src/com.wuss.leetCode/Main42.java
@@ -0,0 +1,55 @@
+package com.wuss.leetCode;
+
+import java.util.*;
+
+/**
+ * 42. 接雨水
+ * @program leetcode
+ * @description:
+ * @author: wuss@wjs.com
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ *
+ *
+ * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 感谢 Marcos 贡献此图。
+ *
+ * 示例:
+ *
+ * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ */
+public class Main42 {
+    //按列求解
+    public int trap(int[] height) {
+        int sum = 0;
+        int[] max_left = new int[height.length];
+        int[] max_right = new int[height.length];
+
+        for (int i = 1; i < height.length - 1; i++) {
+            max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+        }
+        for (int i = 1; i < height.length - 1; i++) {
+            int min = Math.min(max_left[i], max_right[i]);
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        Main42 main42 = new Main42();
+        int[] arr ={0,1,0,2,1,0,1,3,2,1,2,1};
+//        int[] arr ={10527,740,9013,1300,29680,1};
+        System.out.println(main42.trap(arr));
+
+
+    }
+
+    static int all = 0;
+}
