1d5d54867afbc9dee829b964bcce6cd25b7531bc
wuss <XdasA2J9zK_sRJfA8HrW>
2020-04-29 16:59:24 +0800

    格雷编码

---
+++ src/com.wuss.leetCode/Main89.java
@@ -0,0 +1,81 @@
+package com.wuss.leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program leetcode
+ * @description:
+ * @author: wuss@wjs.com
+ * 89. 格雷编码
+ * 格雷编码是一个二进制数字系统，在该系统中，两个连续的数值仅有一个位数的差异。
+ * <p>
+ * 给定一个代表编码总位数的非负整数 n，打印其格雷编码序列。即使有多个不同答案，你也只需要返回其中一种。
+ * <p>
+ * 格雷编码序列必须以 0 开头。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 2
+ * 输出: [0,1,3,2]
+ * 解释:
+ * 00 - 0
+ * 01 - 1
+ * 11 - 3
+ * 10 - 2
+ * <p>
+ * 对于给定的 n，其格雷编码序列并不唯一。
+ * 例如，[0,2,3,1] 也是一个有效的格雷编码序列。
+ * <p>
+ * 00 - 0
+ * 10 - 2
+ * 11 - 3
+ * 01 - 1
+ * 示例 2:
+ * <p>
+ * 输入: 0
+ * 输出: [0]
+ * 解释: 我们定义格雷编码序列必须以 0 开头。
+ * 给定编码总位数为 n 的格雷编码序列，其长度为 2n。当 n = 0 时，长度为 20 = 1。
+ * 因此，当 n = 0 时，其格雷编码序列为 [0]。
+ */
+public class Main89 {
+    /**
+     * 设 nn 阶格雷码集合为 G(n)G(n)，则 G(n+1)G(n+1) 阶格雷码为：
+     * 给 G(n)G(n) 阶格雷码每个元素二进制形式前面添加 00，得到 G'(n)G
+     * ′
+     *  (n)；
+     * 设 G(n)G(n) 集合倒序（镜像）为 R(n)R(n)，给 R(n)R(n) 每个元素二进制形式前面添加 11，得到 R'(n)R
+     * ′
+     *  (n)；
+     * G(n+1) = G'(n) ∪ R'(n)G(n+1)=G
+     * ′
+     *  (n)∪R
+     * ′
+     *  (n) 拼接两个集合即可得到下一阶格雷码。
+     *
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/gray-code/solution/gray-code-jing-xiang-fan-she-fa-by-jyd/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param n
+     * @return
+     */
+    public static List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<>((int)Math.pow(2f,n));
+        list.add(0);
+        int k=1;
+        for (int i=1;i<=n;i++){
+
+            for (int j = list.size()-1;j>=0;j--){
+                list.add(list.get(j)+k);
+            }
+
+            k=k<<1;
+        }
+        return list;
+    }
+
+}
