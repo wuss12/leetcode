6ecd161f367c0f3c20a364a9202be406b28b5f24
wuss <XdasA2J9zK_sRJfA8HrW>
2020-05-13 10:11:38 +0800

    按照层次遍历入参生成二叉树

---
+++ src/com.wuss.leetCode/TreeNode.java
@@ -0,0 +1,48 @@
+package com.wuss.leetCode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+
+    //按照层次遍历入参生成二叉树
+    public static TreeNode getTreeNode(Integer... arr){
+        int len = arr.length;
+        if (len == 0){
+            return null;
+        }
+        int i=0;
+        TreeNode root = new TreeNode(arr[0]);
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.add(root);
+        TreeNode tempNode;
+        while (!deque.isEmpty() && i<len){
+            int size = deque.size();
+            for (int j=0;j<size;j++){
+               tempNode  = deque.pollFirst();
+               if (tempNode == null){
+                   i=i+2;
+                   continue;
+               }
+               if (i+1<len && arr[++i] != null){
+                   tempNode.left = new TreeNode(arr[i]);
+               }
+               if (i+1<len && arr[++i] != null){
+                    tempNode.right = new TreeNode(arr[i]);
+               }
+
+               deque.add(tempNode.left);
+               deque.add(tempNode.right);
+            }
+        }
+        return root;
+    }
+}
