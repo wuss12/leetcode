659ab2e6429bdd9550d8cc1e11e04661cdeb4fdb
wuss <XdasA2J9zK_sRJfA8HrW>
2020-12-23 09:57:14 +0800

    387. 字符串中的第一个唯一字符

---
+++ src/com.wuss.leetCode/Main387.java
@@ -0,0 +1,37 @@
+package com.wuss.leetCode;
+
+/**
+ * @program leetcode
+ * @description:
+ * 387. 字符串中的第一个唯一字符
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ *
+ *
+ *
+ * 示例：
+ *
+ * s = "leetcode"
+ * 返回 0
+ *
+ * s = "loveleetcode"
+ * 返回 2
+ *
+ *
+ * 提示：你可以假定该字符串只包含小写字母。
+ * */
+public class Main387 {
+    public int firstUniqChar(String s) {
+        int[] arr = new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            arr[s.charAt(i)-'a']++ ;
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[s.charAt(i)-'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
